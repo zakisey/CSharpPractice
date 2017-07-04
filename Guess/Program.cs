1e7f86f2585c7c0850be4f33f1c73ad4c9a13850
zakisey <yasuhiiro.sezaki@diva.co.jp>
2017-07-04 14:28:39 +0900

    added Guess project

---
+++ Guess/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Random rand = new System.Random();
+            int ans = rand.Next(100);
+            int count = 0;
+            while (true)
+            {
+                count++;
+                Console.Write("0から100の間の数値を当ててください。> ");
+                string s = Console.ReadLine();
+                int guess;
+                if (!int.TryParse(s, out guess))
+                {
+                    Console.WriteLine("不正な値です。");
+                    return;
+                }
+                if (guess == ans)
+                {
+                    Console.WriteLine("おめでとう。" + count + "回目で当たりました。");
+                    Console.WriteLine();
+                    break;
+                }
+                else if (guess > ans)
+                {
+                    Console.WriteLine("答はもっと小さいです。");
+                }
+                else
+                {
+                    Console.WriteLine("答はもっと大きいです。");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("終了するには何かキーを押してください。");
+            Console.Read();
+        }
+    }
+}
---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   * * * 九九の表 * * *");
+            Console.WriteLine("   |  1  2  3  4  5  6  7  8  9");
+            Console.WriteLine("--------------------------------");
+            for (int row = 1; row < 10; row++)
+            {
+                Console.Write(row + "  |");
+                for (int col = 1; col < 10; col++)
+                {
+                    string space;
+                    if (row * col >= 10)
+                    {
+                        space = " ";
+                    }
+                    else
+                    {
+                        space = "  ";
+                    }
+                    Console.Write(space + row * col);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("終了するには何かキーを押してください。");
+            Console.Read();
+        }
+    }
+}
